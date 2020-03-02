ab581b161896541836ccec44950ebc4ecafb384b
Sebastian <sebaxyala@gmail.com>
2020-03-02 17:27:23 -0500

    Se realiza la primera interfaz grafica

---
+++ interfaces_graficas/primera_interface.py
@@ -0,0 +1,23 @@
+from tkinter import *
+#Construccion de la raiz o ventana 
+raiz = Tk()
+
+
+#si se le quiere dar titulo a esta ventana lo que se hace es lo siguiente
+raiz.title("Mi primer interface")
+
+
+#para cambiar el icono dela ventana se hace de la siguiente manera
+raiz.iconbitmap("@/home/sebastian/Escritorio/Copia_seguridad_Sebastian/Curso_Python/interfaces_graficas/git.xbm")
+
+#para cambiar el tamaño de una venta se pueda de esta manera
+raiz.geometry("650x350")
+
+#este metodo lo que hace es permitir o no que la pestaña se pueda agrandar
+              #ancho #alto
+raiz.resizable(True,False)
+
+
+#esto es como si fuese un bucle infinito que mantiene
+#la ventana abierta y que se rompe al momento de dar click al boton "x"
+raiz.mainloop()
