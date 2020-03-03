70d22d3c668598435af3995d9ee400ad9e7a1bde
Sebastian <sebaxyala@gmail.com>
2020-03-03 07:04:38 -0500

    creacion simple de un frame

--- interfaces_graficas/primera_interface.py
+++ interfaces_graficas/primera_interface.py
@@ -8,6 +8,8 @@ raiz.title("Mi primer interface")
 
 
 #para cambiar el icono dela ventana se hace de la siguiente manera
+#para linux se hace de la siguiente forma:
+#pero para windows solo basta con el nombre del icono o la ruta sin el @ al inicio
 raiz.iconbitmap("@/home/sebastian/Escritorio/Copia_seguridad_Sebastian/Curso_Python/interfaces_graficas/git.xbm")
 
 #para cambiar el tamaño de una venta se pueda de esta manera
@@ -18,9 +20,12 @@ raiz.config(bg="blue")
 
 #este metodo lo que hace es permitir o no que la pestaña se pueda agrandar
               #ancho #alto
-raiz.resizable(True,False)
+raiz.resizable(True,True)
+
+mi_frame = Frame()
 
 
+mi_frame.pack()
 #esto es como si fuese un bucle infinito que mantiene
 #la ventana abierta y que se rompe al momento de dar click al boton "x"
 raiz.mainloop()
